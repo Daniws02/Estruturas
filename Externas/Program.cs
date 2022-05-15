0df37216ef1546dffb946174f33bbb6d7552d4ba
Daniws02 <santanadanilo576@gmail.com>
2022-05-15 13:33:56 -0300

    primeiro commit

---
+++ Externas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Externas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Projeto
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
