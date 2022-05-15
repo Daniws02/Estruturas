45c50b8b0ab1c693e11d3f6dd48b56ccb4673177
Daniws02 <santanadanilo576@gmail.com>
2022-05-15 17:20:45 -0300

    modificando os metodos

--- Metodos/Program.cs
+++ Metodos/Program.cs
@@ -6,7 +6,60 @@ namespace Metodos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Metodo2.Codigo();
+        }
+    }
+    /*
+    class Metodo1
+    {   
+        // Arrumar depois ****
+
+        public static void Codigo()
+        {
+
+            int[] Arr = {0, 0, 0, 0};
+
+            int valor = int.Parse(Console.ReadLine());
+
+            int Solu = Random(Arr, valor);
+
+            Console.WriteLine(Solu);
+
+            
+        }
+        public static int Random(Array Arr, int max)
+        {
+            Random random = new Random();
+            for(int i = 0; i<Arr.Length; i++)
+            {   
+                int P = Arr[i];
+                P = random.Next(max);
+
+                return P;
+            }
+        }
+        
+    }
+    */
+    class Metodo2
+    {
+        // Metodo que localiza um valor e retorna a posição dele
+        public static void Codigo()
+        {
+            int[] Arr = {1, 2, 3, 4};
+
+            int valor = int.Parse(Console.ReadLine());
+
+            int posicao_valor = BinarySearch(Arr, valor);
+
+            Console.WriteLine(posicao_valor);
+        }
+        public static int BinarySearch(Array Arr, int procurado)
+        {
+        
+        int posicao = Array.BinarySearch(Arr, procurado);
+
+        return posicao;
         }
     }
 }
